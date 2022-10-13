e1c1db26406333dfbd231a4bf30133ebd1a7a9b9
chandama <chandler.taylor05@gmail.com>
2022-10-13 08:54:29 -0600

    Update codepipeline.tf

--- codepipeline.tf
+++ codepipeline.tf
@@ -5,8 +5,8 @@
 */
 
 resource "aws_codepipeline" "pipeline" {
-    name                        = "Project1CodePipeline"
-    role_arn                    = aws_iam_role.codepipeline_role.arn
+    name                     = "Project1CodePipeline"
+    role_arn                 = aws_iam_role.codepipeline_role.arn
 
     artifact_store {
       location  = aws_s3_bucket.codepipeline_bucket.bucket
@@ -25,8 +25,8 @@ resource "aws_codepipeline" "pipeline" {
         version             = "1"
 
         configuration = {
-          RepositoryName = aws_codecommit_repository.repo.repository_name
-          BranchName       = "main"
+          RepositoryName    = aws_codecommit_repository.repo.repository_name
+          BranchName        = "main"
         }
       }
     }
@@ -35,15 +35,15 @@ resource "aws_codepipeline" "pipeline" {
       name = "Deploy"
 
       action {
-        name = "Deploy"
-        category = "Deploy"
-        owner = "AWS"
-        provider = "CodeDeploy"
-        version = "1"
-        input_artifacts = ["source_output"]
+        name                = "Deploy"
+        category            = "Deploy"
+        owner               = "AWS"
+        provider            = "CodeDeploy"
+        version             = "1"
+        input_artifacts     = ["source_output"]
 
         configuration = {
-          ApplicationName = aws_codedeploy_app.ice_cream_app.name
+          ApplicationName   = aws_codedeploy_app.ice_cream_app.name
           DeploymentGroupName = aws_codedeploy_deployment_group.deployment-group.deployment_group_name
         }
       } 
